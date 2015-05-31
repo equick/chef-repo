e0ff665b905075a0a936fbf837075cc8e876c4ab
equick <edwardquick@hotmail.com>
2015-05-31 23:23:11 +0100

    adding customised tomcat cookbook

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name             'my_cookbook'
+maintainer       'Edward Quick'
+maintainer_email 'quick@linuxproblems.org'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends		'tomcat' 
